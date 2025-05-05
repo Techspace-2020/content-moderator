234abb413465b9e63a7b6473bd1cec0cd2353fc4
Rakesh C P <rakeshcp85@gmail.com>
2025-05-05 13:46:26 +0530

    Updated host

--- barrier_ai_api.py
+++ barrier_ai_api.py
@@ -480,4 +480,4 @@ def analyze_message(input: MessageInput):
 
 # Run the FastAPI app if this script is executed directly
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
